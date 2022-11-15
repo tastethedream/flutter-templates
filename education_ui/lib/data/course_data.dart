a07ecbce75acb08674c2fd2a6fee08492cb3db92
Dawn Rose <tastethedream@gmail.com>
2022-11-15 12:54:18 +0000

    Add: new file for course data

---
+++ education_ui/lib/data/course_data.dart
@@ -0,0 +1,38 @@
+class DataItem{
+  final String title;
+
+  final String category;
+  final String imageUrl;
+  final String mentor;
+  final String mentorImageUrl;
+
+  DataItem({required this.title, required this.category, required this.imageUrl, required this.mentor, required this.mentorImageUrl});
+
+}
+
+class CourseData {
+  final List<DataItem> items = [
+     DataItem(
+      title: 'Data Analyst',
+      imageUrl: 'https://images.unsplash.com/photo-1599658880436-c61792e70672?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZGF0YSUyMGFuYWx5c3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+      category: 'Data Science',
+      mentor: 'Bill Smith',
+      mentorImageUrl: 'https://icons8.com/icon/EvfjU4d05f0I/circled-user-male-skin-type-6',
+     ),
+    DataItem(
+      title: 'Data Analyst',
+      imageUrl: 'https://images.unsplash.com/photo-1599658880436-c61792e70672?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZGF0YSUyMGFuYWx5c3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+      category: 'Data Science',
+      mentor: 'Bill Smith',
+      mentorImageUrl: 'https://icons8.com/icon/EvfjU4d05f0I/circled-user-male-skin-type-6',
+    ),
+    DataItem(
+      title: 'Data Analyst',
+      imageUrl: 'https://images.unsplash.com/photo-1599658880436-c61792e70672?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZGF0YSUyMGFuYWx5c3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+      category: 'Data Science',
+      mentor: 'Bill Smith',
+      mentorImageUrl: 'https://icons8.com/icon/EvfjU4d05f0I/circled-user-male-skin-type-6',
+    ),
+  ];
+
+}
